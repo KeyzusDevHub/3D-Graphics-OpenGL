555a34d8a8625c3fe5fcaecdf1fc3940ac253846
KeyzusDevHub <147584729+KeyzusDevHub@users.noreply.github.com>
2023-10-11 08:46:16 +0200

    Complete 01_House, 02_Colors, 03_Indices

---
+++ src/Assignments/04_Uniforms/shaders/base_fs.glsl
@@ -0,0 +1,14 @@
+#version 420
+
+layout(location=0) out vec4 vFragColor;
+layout(std140, binding = 0) uniform Mixer {
+    float strength;
+    vec3  mix_color; 
+};
+
+
+in vec4 vertex_color;
+
+void main() {
+    vFragColor = vertex_color;
+}
