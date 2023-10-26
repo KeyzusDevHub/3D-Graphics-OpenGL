64273cf63232a46242230105a5dd898b7cc73f20
TrinianGit <94129735+TrinianGit@users.noreply.github.com>
2023-10-26 21:18:53 +0200

    Complete 11_KdMaterial

--- src/Assignments/11_KdMaterial/app.cpp
+++ src/Assignments/11_KdMaterial/app.cpp
@@ -63,22 +63,28 @@ void SimpleShapeApplication::init() {
         13, 14, 15
     };
 
+    // Create and set new Camera object
     set_camera(new xe::Camera);
+    // Create and set new CameraController object
     set_controler(new xe::CameraController(camera()));
 
-
-
+    // Create new mesh object
     auto pyramid = new xe::Mesh(6 * sizeof(float), vertices.size() * sizeof(float), GL_STATIC_DRAW,
                              indices.size() * sizeof(GLubyte), GL_UNSIGNED_BYTE, GL_STATIC_DRAW);
 
+    // Load vertices to mesh object
     pyramid->load_vertices(0, vertices.size() * sizeof(GLfloat), vertices.data());
 
+    // Adding position attribute to mesh
     pyramid->add_attribute(xe::POSITION, 3, GL_FLOAT, 0);
 
+    // Adding color attribute to mesh
     pyramid->add_attribute(xe::COLOR_0, 3, GL_FLOAT, 3 * sizeof(GLfloat));
 
+    // Load indices to mesh object
     pyramid->load_indices(0, indices.size() * sizeof(GLubyte), indices.data());
 
+    // Creating different Materials (different colors) and adding them to vertices
     auto kd_gray_material = new xe::KdMaterial(glm::vec4(0.5f, 0.5f, 0.5f, 1.0f));
     
     pyramid->add_primitive(0, 6, kd_gray_material);
@@ -99,8 +105,10 @@ void SimpleShapeApplication::init() {
 
     pyramid->add_primitive(15, 18, kd_blue_material);
 
+    // Add mesh to list
     add_mesh(pyramid);
 
+    // Generating empty uniform buffer
     OGL_CALL(glGenBuffers(1, &u_trans_buffer_handle_));
     OGL_CALL(glBindBuffer(GL_UNIFORM_BUFFER, u_trans_buffer_handle_));
     OGL_CALL(glBufferData(GL_UNIFORM_BUFFER, 16 * sizeof(float), NULL, GL_STATIC_DRAW));
@@ -111,11 +119,11 @@ void SimpleShapeApplication::init() {
 
     M_ = glm::mat4(1.0f);
 
+    // Setting camera position and view angle
     camera()->look_at(glm::vec3(2.0f, 1.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
     camera()->perspective(glm::radians(45.0f), (GLfloat) w / (GLfloat) h, 0.1f, 20.0f);
 
     // Setting the background color of the rendering window,
-    // I suggest not using white or black for better debugging.
     OGL_CALL(glClearColor(0.81f, 0.81f, 0.8f, 1.0f));
 
     // This set up an OpenGL viewport of the size of the whole rendering window.
@@ -127,26 +135,32 @@ void SimpleShapeApplication::init() {
 //This functions is called every frame and does the actual rendering.
 void SimpleShapeApplication::frame() {
 
+    // Creating PVM matrix from camera data and model matrix
     glm::mat4 PVM = camera()->projection() * camera()->view() * M_;
 
     OGL_CALL(glBindBufferBase(GL_UNIFORM_BUFFER, 1, u_trans_buffer_handle_));
+    // Sending PVM matrix to uniform buffer
     OGL_CALL(glBufferSubData(GL_UNIFORM_BUFFER, 0, sizeof(glm::mat4), &PVM[0]));
+    // Draw all meshes in mesh list
     for (auto m: meshes_)
         m->draw();
     OGL_CALL(glBindBufferBase(GL_UNIFORM_BUFFER, 1, 0));
 }
 
+// Resize function for OpenGL window
 void SimpleShapeApplication::framebuffer_resize_callback(int w, int h) {
     glViewport(0, 0, w, h);
     Application::framebuffer_resize_callback(w, h);
     camera()->set_aspect((GLfloat) w / (GLfloat) h);
 }
 
+// Scroll handling function for OpenGL window
 void SimpleShapeApplication::scroll_callback(double xoffset, double yoffset) {
     Application::scroll_callback(xoffset, yoffset);   
     camera()->zoom(yoffset / -20.0f);
 }
 
+// Mouse click handling function for OpenGL window
 void SimpleShapeApplication::mouse_button_callback(int button, int action, int mods) {
     Application::mouse_button_callback(button, action, mods);
 
@@ -162,6 +176,7 @@ void SimpleShapeApplication::mouse_button_callback(int button, int action, int m
     }
 }
 
+// Cursor movement handling function for OpenGL window
 void SimpleShapeApplication::cursor_position_callback(double x, double y) {
     Application::cursor_position_callback(x, y);
     if (controller_) {
